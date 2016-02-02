6fdf3229eb5090692670707ce044b1934c5535d3
Thiago Lunardi <thiago@thiagolunardi.net>
2016-02-02 10:34:22 -0200

    Implementando API com Identity Isolation

---
+++ src/EP.IdentityIsolation.API/Startup.cs
@@ -0,0 +1,15 @@
+﻿using EP.IdentityIsolation.API;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(Startup))]
+namespace EP.IdentityIsolation.API
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
